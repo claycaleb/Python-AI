12845c13e3fab023e29ae25665ab9bf22f341c66
claycaleb <calebaclay@gmail.com>
2020-05-13 00:17:11 -0400

    recognize_speech.py

--- recognize_speech.py
+++ recognize_speech.py
@@ -1,19 +1,15 @@
 import speech_recognition as sr
-import pyttsx3
+from ibm_watson import SpeechToTextV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import credentials
 
 def recognize_speech_from_mic(recognizer, microphone):
-    # https://realpython.com/python-speech-recognition/#working-with-microphones
-    """Transcribe speech from recorded from `microphone`.
 
-    Returns a dictionary with three keys:
-    "success": a boolean indicating whether or not the API request was
-               successful
-    "error":   `None` if no error occurred, otherwise a string containing
-               an error message if the API could not be reached or
-               speech was unrecognizable
-    "transcription": `None` if speech could not be transcribed,
-               otherwise a string containing the transcribed text
-    """
+    authenticator = IAMAuthenticator(credentials.IBM_KEY)
+    speech_to_text = SpeechToTextV1(authenticator = authenticator)
+
+    speech_to_text.set_service_url(credentials.IBM_URL)
+
     # check that recognizer and microphone arguments are appropriate type
     if not isinstance(recognizer, sr.Recognizer):
         raise TypeError("`recognizer` must be `Recognizer` instance")
@@ -21,50 +17,11 @@ def recognize_speech_from_mic(recognizer, microphone):
     if not isinstance(microphone, sr.Microphone):
         raise TypeError("`microphone` must be `Microphone` instance")
 
-    # adjust the recognizer sensitivity to ambient noise and record audio
-    # from the microphone
     with microphone as source:
-        # recognizer.adjust_for_ambient_noise(source)
+        recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
+    speech_recognition_results = speech_to_text.recognize(audio = audio.get_wav_data(), content_type = 'audio/wav').get_result()
+    transcript = speech_recognition_results['results'][0]['alternatives'][0]['transcript']
 
-    # set up the response object
-    response = {
-        "success": True,
-        "error": None,
-        "transcription": None
-    }
-
-    # try recognizing the speech in the recording
-    # if a RequestError or UnknownValueError exception is caught,
-    #     update the response object accordingly
-    try:
-        response["transcription"] = recognizer.recognize_google(audio)
-    except sr.RequestError:
-        # API was unreachable or unresponsive
-        response["success"] = False
-        response["error"] = "API unavailable"
-    except sr.UnknownValueError:
-        # speech was unintelligible
-        response["error"] = "Unable to recognize speech"
-
-    return response
-
-def artie_says(phrase):
-    artie = pyttsx3.init()
-    artie.say(phrase)
-    artie.runAndWait()
-
-def main():
-
-    r = sr.Recognizer()
-    mic = sr.Microphone()
-
-    guess = recognize_speech_from_mic(r, mic)
-    print(guess["transcription"])
-
-    artie_says(f'You just said, {guess["transcription"]}')
-
-
-if __name__ == "__main__":
-    main()
+    return transcript
 
